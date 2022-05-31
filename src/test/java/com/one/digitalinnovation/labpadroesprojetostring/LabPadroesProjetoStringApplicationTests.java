b4d0c3dd562222da8e7a0fde37266971527e2925
karinevitoria <karinevfalves@gmail.com>
2022-05-31 19:52:30 -0300

    primeiro commit

---
+++ src/test/java/com/one/digitalinnovation/labpadroesprojetostring/LabPadroesProjetoStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.labpadroesprojetostring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
